1c7363be1caec296b73092298d92ceb4d740c0fe
suegary <college.sujiali@gmail.com>
2013-05-29 18:27:50 +0800

    1

--- WindowsFormsApplication2/WindowsFormsApplication2/NewFolder1/Form1.cs
+++ WindowsFormsApplication2/WindowsFormsApplication2/NewFolder1/Form1.cs
@@ -7,11 +7,11 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
-namespace WindowsFormsApplication1.UIL
+namespace WindowsFormsApplication2.NewFolder1
 {
-    public partial class _123 : Form
+    public partial class Form1 : Form
     {
-        public _123()
+        public Form1()
         {
             InitializeComponent();
         }

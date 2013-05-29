1bfd5b624a269e5a8592e3e92d6738ab0f0757f2
suegary <college.sujiali@gmail.com>
2013-05-29 19:59:54 +0800

    123

--- WindowsFormsApplication1/WindowsFormsApplication1/G盘.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/G盘.Designer.cs
@@ -39,14 +39,15 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // Form2
+            // G盘
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
             this.Controls.Add(this.button1);
-            this.Name = "Form2";
+            this.Name = "G盘";
             this.Text = "Form2";
             this.ResumeLayout(false);
 

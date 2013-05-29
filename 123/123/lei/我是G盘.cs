2faa265a010cc15b6a5c935a96add8baa5b21b3a
suegary <college.sujiali@gmail.com>
2013-05-29 19:00:16 +0800

    123

---
+++ 123/123/lei/我是G盘.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _123.lei
+{
+    public partial class 我是G盘 : Form
+    {
+        public 我是G盘()
+        {
+            InitializeComponent();
+        }
+    }
+}
